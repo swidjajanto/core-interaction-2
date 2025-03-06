4036b2d6711e843efe2e2eb1f64fe0c62c84ce0c
swidjajanto <widjs264@newschool.edu>
2025-03-06 10:41:47 -0500

    penny-presser

--- studio/workshops/penny-presser/scripts.js
+++ studio/workshops/penny-presser/scripts.js
@@ -1,6 +1,7 @@
 // get the elements
 
 var pressButton = document.querySelector('button[name="press"]');
+var studentSelect = document.querySelector('select[name="student"]');
 var pennySelect = document.querySelector('select[name="penny"]');
 var pennyImage = document.querySelector('.penny');
 
@@ -13,11 +14,15 @@ console.log(pennySelect.value);
 
 pressButton.addEventListener('click', () => {
 
+    //log the student value 
+    console. log(studentSelect.value);
+
     // log the penny value
     console.log(pennySelect.value);
 
     //set the penny image src
-    pennyImage.src = 'img/' + pennySelect.value;
+   // pennyImage.src = 'img/' + pennySelect.value;
+   pennyImage.src = 'https://' + studentSelect.value + '.interactive.rodeo/studio/workshops/penny-presser/img/' + pennySelect.value;
 
     // log the penny image
     console.log(pennyImage);
